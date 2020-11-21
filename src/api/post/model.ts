585ad5c17e9796c1cb30e7377c73b058c763d69e
Danielmagox <daniel1305972@gmail.com>
2020-11-21 12:39:37 +0000

    ADDED models

---
+++ src/api/post/model.ts
@@ -0,0 +1,27 @@
+import { ObjectId } from 'mongodb';
+
+export interface Post {
+	owner: {
+	_id: ObjectId;
+	username: string;
+	}
+name: string;
+description: string;
+time: number;
+servings: number;
+ingredients: [ Ingredient ];
+steps: [strings]
+creation: Date;
+likes: number;
+comments: number;
+tags: [ string ];
+}
+
+export interface Ingredient {
+	name: string;
+	allergen: Allergen;
+}
+
+export interface Allergen {
+name: string;
+}
