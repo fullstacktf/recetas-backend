1e503b0fc8c3f459b89facda4d649da14f745819
Danielmagox <daniel1305972@gmail.com>
2020-11-21 12:35:47 +0000

    COMMENTED line

--- src/databaseUtils.ts
+++ src/databaseUtils.ts
@@ -1,29 +1,30 @@
-import mongoose from "mongoose";
-import { Db } from "mongodb";
+import mongoose from 'mongoose';
+import { Db } from 'mongodb';
 
 export let database: Db;
 
 export function connectDatabase(): Promise<Db> {
-  const username = "";
-  const password = "";
-  const host = "localhost";
-  const port = "27017";
-  const databaseName = "snapfork";
+  const username = '';
+  const password = '';
+  const host = 'localhost';
+  const port = '27017';
+  const databaseName = 'snapfork';
   return new Promise<Db>((resolve, reject) => {
-    const url = `mongodb://${username}:${password}@${host}:${port}/${databaseName}`;
+    const url = 'mongodb://'/*${username}:${password}@*/ + `${host}:${port}/${databaseName}`;
     mongoose.connect(url, {
       useNewUrlParser: true,
+      useUnifiedTopology: true
     });
 
     const connection = mongoose.connection;
 
-    connection.on("error", function (err) {
-      console.error("Error connecting to DB", err);
-      return reject("Error connecting to DB");
+    connection.on('error', function (err) {
+      console.error('Error connecting to DB', err);
+      return reject('Error connecting to DB');
     });
 
-    connection.once("open", function () {
-      console.log("Connection to DB successful");
+    connection.once('open', function () {
+      console.log('Connection to DB successful');
       database = connection.db;
       return resolve(database);
     });
