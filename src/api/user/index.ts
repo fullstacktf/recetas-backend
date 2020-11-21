d2333af189d69d0035b3e4853a41665ebef431c1
Imar Abreu Díaz <abreudiazimar9620@gmail.com>
2020-11-21 13:30:38 +0000

    ADD initial user endpoints

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,1 +1,29 @@
 import express from 'express';
+
+const router = express.Router();
+
+router.post('/login', (req, res) => {
+  res.json();
+});
+
+router.post('/register', (req, res) => {
+  res.json();
+});
+
+router.get(':userID/profile', (req, res) => {
+  res.json();
+});
+
+// middleware USUARIOS LOGUEADOS
+
+router.put('/password/reset', (req, res) => {
+  res.json();
+});
+
+router.get('/:userID/timeline', (req, res) => {
+  res.json();
+});
+
+router.put(':userID/profile', (req, res) => {
+  res.json();
+});
