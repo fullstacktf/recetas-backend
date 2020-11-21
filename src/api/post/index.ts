29f80b7759535c346bfc6ce7a4d4200e6844bbbf
Imar Abreu Díaz <abreudiazimar9620@gmail.com>
2020-11-21 14:55:09 +0000

    ADD post endpoints

--- src/api/post/index.ts
+++ src/api/post/index.ts
@@ -2,36 +2,60 @@ import express from 'express';
 
 const router = express.Router();
 
-/*
-	GET     api/v1/posts/public/popular
-    GET		api/v1/posts/{PostID}
-    POST	api/v1/posts/{postID}/like
-    DELETE	api/v1/posts/{postID}/like
-    POST	api/v1/posts/{postID}/comment
-    PUT		api/v1/posts/{postID}/comment
-    DELETE	api/v1/posts/{postID}/comment
-    POST	api/v1/posts/{postID}/save
-    DELETE	api/v1/posts/{postID}/save
-	GET		api/v1/posts/tag/{tagID}
-
-	GET		api/v1/posts/{postID}/save
-	POST	api/v1/posts
-    PUT		api/v1/posts/{postID}
-    DELETE	api/v1/posts/{postID}
-*/
-
 router.get('/public/popular', (req, res) => {
   res.json();
 });
 
-router.get('/login', (req, res) => {
+router.get('/:postID', (req, res) => {
   res.json();
 });
 
-router.get('/login', (req, res) => {
+router.get('/tag/:tagID', (req, res) => {
   res.json();
 });
 
-router.get('/login', (req, res) => {
+// middleware USUARIOS LOGUEADOS
+
+router.post('/:postID/like', (req, res) => {
+  res.json();
+});
+
+router.delete('/:postID/like', (req, res) => {
+  res.json();
+});
+
+router.post('/:postID/comment', (req, res) => {
+  res.json();
+});
+
+router.put('/:postID/comment', (req, res) => {
+  res.json();
+});
+
+router.delete('/:postID/comment', (req, res) => {
+  res.json();
+});
+
+router.post('/:postID/save', (req, res) => {
+  res.json();
+});
+
+router.delete('/:postID/save', (req, res) => {
+  res.json();
+});
+
+router.get('/:postID/save', (req, res) => {
+  res.json();
+});
+
+router.post('/', (req, res) => {
+  res.json();
+});
+
+router.put('/:postID', (req, res) => {
+  res.json();
+});
+
+router.delete('/:postID', (req, res) => {
   res.json();
 });
