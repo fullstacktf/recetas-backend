e912d3f7ac09fc4ecf0706eb6c889f888fc5ee25
Imar Abreu Díaz <abreudiazimar9620@gmail.com>
2020-11-30 12:35:20 +0000

    ADD method for crud comments

--- src/api/comment/controller.ts
+++ src/api/comment/controller.ts
@@ -1,5 +1,20 @@
 import { ObjectId } from 'mongodb';
-import { Comment } from './model/comment';
+import { CommentModel, Comment } from './model/comment';
+
+export const createComment = (comment: CommentModel) => {
+  comment.user._id = new ObjectId(comment.user._id);
+  const newComment = new Comment(comment);
+  return newComment.save();
+};
+
+export const removeComment = (commentID: string) => {
+  const _id = new ObjectId(commentID);
+  return Comment.findByIdAndDelete({ _id });
+};
+
+export const removeCommentByPostId = (postID: ObjectId) => {
+  return Comment.deleteMany({ postID: postID });
+};
 
 export const addLike = (id: string) => {
   const _id = new ObjectId(id);
