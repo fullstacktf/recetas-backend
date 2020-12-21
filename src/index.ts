c014f2d0d7a658e77d660944e7f457ed69b9b092
Imar Abreu Díaz <abreudiazimar9620@gmail.com>
2020-12-21 21:26:25 +0000

    REMOVE old cors

--- src/index.ts
+++ src/index.ts
@@ -24,20 +24,6 @@ app.use(passport.initialize());
 passport.use(jwtStrategy);
 app.use(fileUpload());
 
-// Add headers
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', ['http://localhost:3001']);
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  // Pass to next layer of middleware
-  next();
-});
-
 app.use('/static', express.static('/public'));
 
 app.use('/user', userRouter);
